9af1305099c4512c338e5118ccb010379cb9be62
Tyler Culp <tylercculp@gmail.com>
2018-11-20 13:53:08 -0500

    Removed uploadArtifacts block

--- src/main/groovy/io/ossim/gradleDefaults/UploadArtifacts.groovy
+++ src/main/groovy/io/ossim/gradleDefaults/UploadArtifacts.groovy
@@ -4,14 +4,6 @@ import org.gradle.api.Project
 
 class UploadArtifacts {
     static void addUploadArtifact(Project project) {
-        uploadArchives {
-            repositories {
-                mavenDeployer {
-                    repository(url: "${System.env.MAVEN_REPOSITORY_URL}/omar-local-${buildVersionTag.toLowerCase()}") {
-                        authentication(userName: "${System.env.MAVEN_REPOSITORY_USER}", password: "${System.env.MAVEN_REPOSITORY_PASSWORD}")
-                    }
-                }
-            }
-        }
+
     }
 }
