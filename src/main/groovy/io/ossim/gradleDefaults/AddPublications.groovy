be6071ad5edc70751cd2843c1529614e528e1470
Tyler Culp <tylercculp@gmail.com>
2018-11-20 15:47:46 -0500

    Switching to project.with

--- src/main/groovy/io/ossim/gradleDefaults/AddPublications.groovy
+++ src/main/groovy/io/ossim/gradleDefaults/AddPublications.groovy
@@ -6,23 +6,23 @@ import org.gradle.api.publish.maven.MavenPublication
 class AddPublications {
     static void addPublication(Project project) {
         project.apply plugin: "maven-publish"
-        def publishingClosure = {
-            publications {
-                "${name}" ( MavenPublication ) {
-                    artifact( file( "${ project.buildDir }/libs/${ project.name }-${ project.version }.jar" ) )
-                }
-            }
-            repositories {
-                maven {
-                    credentials {
-                        username System.getenv( 'MAVEN_REPOSITORY_USERNAME' )
-                        password System.getenv( 'MAVEN_REPOSITORY_PASSWORD' )
+        project.with {
+            publishing {
+                publications {
+                    project.name(MavenPublication) {
+                        artifact(file("${buildDir}/libs/${project.name}-${project.version}.jar"))
                     }
-                    url = "${ System.getenv( 'MAVEN_REPOSITORY_URL' ) }/o2-${project.buildReleaseTag}"
+                }
+                repositories {
+                    maven {
+                        credentials {
+                            username System.getenv('MAVEN_REPOSITORY_USERNAME')
+                            password System.getenv('MAVEN_REPOSITORY_PASSWORD')
+                        }
+                        url = "${System.getenv('MAVEN_REPOSITORY_URL')}/o2-${project.buildReleaseTag}"
+                    }
                 }
             }
         }
-        publishingClosure.setDelegate(project)
-        project.publishing(publishingClosure)
     }
 }
