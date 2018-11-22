f5c1f106290bb9f91a055e5f50487b0c85f763c9
Tyler Culp <tylercculp@gmail.com>
2018-11-21 20:42:39 -0500

    Compile sonarQube plugin into this one

--- src/main/groovy/io/ossim/gradleDefaults/SonarQube.groovy
+++ src/main/groovy/io/ossim/gradleDefaults/SonarQube.groovy
@@ -4,9 +4,6 @@ import org.gradle.api.Project
 
 class SonarQube {
     static void addSonarQube(Project project){
-        project.dependencies {
-            compile "org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:2.5"
-        }
         project.apply plugin: "org.sonarqube:2.6.2"
     }
 }
