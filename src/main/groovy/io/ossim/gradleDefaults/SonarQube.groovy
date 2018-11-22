d69a63f327da122092c076c02b561f4fce24b87e
Tyler Culp <tylercculp@gmail.com>
2018-11-21 20:38:15 -0500

    Changed classpath to compile

--- src/main/groovy/io/ossim/gradleDefaults/SonarQube.groovy
+++ src/main/groovy/io/ossim/gradleDefaults/SonarQube.groovy
@@ -5,7 +5,7 @@ import org.gradle.api.Project
 class SonarQube {
     static void addSonarQube(Project project){
         project.dependencies {
-            classpath "org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:2.5"
+            compile "org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:2.5"
         }
         project.apply plugin: "org.sonarqube:2.6.2"
     }
